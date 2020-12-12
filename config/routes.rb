53dfa0db7e221e8556bb3fbe64777d90719e7147
Jakob Persons <jakobep24@gmail.com>
2020-12-11 20:33:50 -0500

    made changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :new, :create]
+  # resources :posts, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # draw out routes individually
+  # index
+  get "/posts", to: "posts#index", as: "posts"
+
+  # show
+  get "/posts/:id", to: "posts#show", as: "post"
+
+  #new
+  get "/posts/new", to: "posts#new", as: "new_post"
+
+  #create - '/posts' already exists --> no need for 'as:'
+  post "/posts", to: "posts#create"
 end
